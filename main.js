d5b006de4f34efe363269ff141256ecbf1b7872b
LorenzoAlloggio <l.alloggio@sintlucasedu.nl>
2024-02-08 11:55:26 +0100

    up

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ const gameWindow = document.getElementById("gameWindow");
 
 //Main Character Reference
 const mainCharacter = document.getElementById("mainCharacter");
+const offsetCharacter = 16;
 
 gameWindow.onclick = function(e){
+    var rect = gameWindow.getBoundingClientRect();
+    var X = e.clientX - rect.left;
+    var Y = e.clientY - rect.top;
     console.log(e.target.id);
+    mainCharacter.style.left = X - offsetCharacter + "px";
+    mainCharacter.style.top = Y - offsetCharacter + "px";
 }
