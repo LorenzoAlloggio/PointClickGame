80083998bd040e7e04f4adc2caaf68d584ad2438
LorenzoAlloggio <l.alloggio@sintlucasedu.nl>
2024-02-08 10:57:40 +0100

    up

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 document.getElementById("title").innerText = "Point and click adventure game"
+
+//Game windows reference
+const gameWindow = document.getElementById("gameWindow");
+
+//Main Character Reference
+const mainCharacter = document.getElementById("mainCharacter");
+
+gameWindow.onclick = function(e){
+    console.log(e.target.id);
+}
