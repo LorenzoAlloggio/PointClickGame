0bc68b823c07199f7dee1eb6d7ec2ca2eeff6be1
LorenzoAlloggio <l.alloggio@sintlucasedu.nl>
2024-02-19 10:51:25 +0100

    up

--- main.js
+++ main.js
@@ -3,14 +3,23 @@ document.getElementById("title").innerText = "Point and click adventure game"
 //Game windows reference
 const gameWindow = document.getElementById("gameWindow");
 
+//Game stats
+gamestate = {
+    "door2locked": true,
+};
+
 //Main Character Reference
 const mainCharacter = document.getElementById("mainCharacter");
 const offsetCharacter = 16;
 
+//Inventory
+const inventoryBox = document.getElementById("inventoryBox");
+const inventoryList = document.getElementById("inventoryList");
+
 //Foreground items
 const door1 = document.getElementById("door1");
 
-gameWindow.onclick = function(e){
+gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var X = e.clientX - rect.left;
     var Y = e.clientY - rect.top;
@@ -18,28 +27,48 @@ gameWindow.onclick = function(e){
     mainCharacter.style.left = X - offsetCharacter + "px";
     mainCharacter.style.top = Y - offsetCharacter + "px";
 
-    switch(e.target.id){
-
-        case "key1":
-            console.log("found key")
-            break;
+    switch (e.target.id) {
 
         case "door1":
             mainCharacter.style.backgroundColor = "#FF0000"
             door1.style.opacity = 0.5;
+            if (document.getElementById("key1") !== null) {
+                console.log("found key")
+                document.getElementById('key1').remove()
+                const keyElement = document.createElement('LI');
+                keyElement.id = "Inv-key";
+                keyElement.innerText = "key";
+                inventoryList.appendChild(keyElement);
+            }
             break;
 
-            case "sign":
-                mainCharacter.style.backgroundColor = "#FF0000"
-                door1.style.opacity = 0.5;
-                sign.style.opacity = 1;
+            case "door2":
+                // check if we have key
+                if(gamestate.door2locked == true){
+                if(document.getElementById("Inv-key") !== null){
+                    //yes -> unlock door
+                    gamestate.door2locked = false;
+                    console.log("Door unlocked")
+                }else{
+                        //no -> alert 'door locked'
+                        alert("Door is locked");
+                    }
+                }else{
+                    console.log("Enter Building")
+                }
                 break;
-            
-            default:
-                //explode
-                mainCharacter.style.backgroundColor = "#0084ff";
-        door1.style.opacity = 1;
-        sign.style.opacity = 1;
-                break;
+
+        case "sign":
+            mainCharacter.style.backgroundColor = "#FF0000"
+            door1.style.opacity = 0.5;
+            sign.style.opacity = 1;
+            break;
+
+        default:
+            //explode
+            mainCharacter.style.backgroundColor = "#0084ff";
+            door1.style.opacity = 1;
+            sign.style.opacity = 1;
+            break;
     }
 }
