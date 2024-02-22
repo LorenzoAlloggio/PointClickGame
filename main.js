9347076d911e9ad8ff60314e202f2511ca414a50
LorenzoAlloggio <l.alloggio@sintlucasedu.nl>
2024-02-22 12:58:56 +0100

    update

--- main.js
+++ main.js
@@ -1,63 +1,65 @@
 document.getElementById("title").innerText = "Point and Click Adventure game";
 //game window reference
 const gameWindow = document.getElementById("gameWindow");
-
+ 
 //Game state
 let gameState = {
     "door2locked": true,
     "inventory": [
-
+ 
     ]
-
+ 
 }
-
-const sec = 4000; // time in milliseconds
-
+const sec = 1000;
+ 
 //Main Character
 const mainCharacter = document.getElementById("mainCharacter");
 const offsetCharacter = 16;
-
+ 
 //speech bubbles
-const mainCharacterSpeech = document.getElementById("mainCharacterSpeech")
-const counterSpeech = document.getElementById("counterSpeech")
-
+const mainCharacterSpeech = document.getElementById("mainCharacterSpeech");
+const counterSpeech = document.getElementById("counterSpeech");
+const counterAvatarImg = document.getElementById("counterAvatarImg");
+const mcAudio = document.getElementById("mcAudio");
+const counterAudio = document.getElementById("counterAudio");
+ 
 //Inventory
 const inventoryBox = document.getElementById("inventoryBox")
 const inventoryList = document.getElementById("inventoryList")
-
+ 
 //Foreground items
 const door1 = document.getElementById("door1");
 const sign = document.getElementById("sign");
-
+ 
 updateInventory(gameState.inventory, inventoryList);
-
-
+ 
+ 
 gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
-
+ 
     if (e.target.id !== "mcImage") {
         mainCharacter.style.left = x - offsetCharacter + "px";
         mainCharacter.style.top = y - offsetCharacter + "px";
     }
-
-
+ 
+ 
     console.log(e.target.id);
     switch (e.target.id) {
         case "door1":
-
+ 
             door1.style.opacity = 0.5;
             sign.style.opacity = 1;
-
+ 
             if (document.getElementById("key1") !== null) {
                 console.log("Found Key!");
                 document.getElementById("key1").remove();
                 changeInventory('Key', 'add');
-
-
+ 
+ 
             } break;
-
+ 
         case "door2":
             if (gameState.door2locked == true) {
                 //check wether we have key
@@ -66,7 +68,7 @@ gameWindow.onclick = function (e) {
                     gameState.door2locked = false;
                     changeInventory('Key', 'delete');
                     console.log("Door is now open.");
-
+ 
                 } else {
                     //no -> alert 'door locked'
                     alert('Door is locked');
@@ -74,31 +76,45 @@ gameWindow.onclick = function (e) {
             } else {
                 console.log('Enter building');
             }
-
+ 
             break;
-
-
-
+ 
+ 
+ 
         case "sign":
-
             sign.style.opacity = 0.5;
             door1.style.opacity = 1;
-
+ 
             break;
-
+ 
+        case "statue":
+            showMessage(mainCharacterSpeech, mcAudio, "Wow, a cool statue!");
+            setTimeout(function () { counterAvatarImg.style.opacity = 1 }, 2 * sec);
+            setTimeout(showMessage, 4 * sec, counterSpeech, counterAudio, "Hello... Person");
+            setTimeout(showMessage, 8 * sec, mainCharacterSpeech, mcAudio, "YOU CAN TALK?!");
+            setTimeout(showMessage, 12 * sec, counterSpeech, counterAudio, "Yes.. wait you can hear me?");
+            setTimeout(showMessage, 16 * sec, mainCharacterSpeech, mcAudio, "Yes i can! this is amazing.");
+            setTimeout(showMessage, 20 * sec, counterSpeech, counterAudio, "This has never happened before..");
+            setTimeout(showMessage, 24 * sec, mainCharacterSpeech, mcAudio, "Could you help me find the key to the left house?");
+            setTimeout(showMessage, 28 * sec, counterSpeech, counterAudio, "Go to the well, there is a key hidden in there.");
+            setTimeout(showMessage, 32 * sec, mainCharacterSpeech, mcAudio, "Thank you!!");
+ 
+            setTimeout(function () { counterAvatarImg.style.opacity = 0 }, 36 * sec);
+            break;
+ 
         default:
             //explode
             door1.style.opacity = 1;
             sign.style.opacity = 1;
             break;
-
+ 
     }
-
+ 
     updateInventory(gameState.inventory, inventoryList);
-
-
+ 
+ 
 }
-
+ 
 /**
  * function to change inventory
  * @param {string} itemName
@@ -110,13 +126,13 @@ function changeInventory(itemName, action) {
         console.log("Wrong parameters given to changeInventory");
         return
     }
-
+ 
     switch (action) {
         case 'add':
             gameState.inventory.push(itemName);
-
+ 
             break;
-
+ 
         case 'delete':
             gameState.inventory.find(function (item, index) {
                 if (item == itemName) {
@@ -124,11 +140,11 @@ function changeInventory(itemName, action) {
                     if (index !== -1) {
                         gameState.inventory.splice(index, 1);
                     }
-
+ 
                 }
             })
             break;
-
+ 
         default:
             break;
     }
@@ -146,35 +162,41 @@ function updateInventory(inventory, inventoryList) {
         inventoryItem.id = "inv-" + item;
         inventoryItem.innerText = item;
         inventoryList.appendChild(inventoryItem);
-
+ 
     })
-
-
-
+ 
+ 
+ 
 }
+ 
 /**
- * Show a message in a speech Bubble
- * @param {*} targetBalloon
- * @param {string} message 
+ * Shows a message in the speech bubble
+ * @param {getElementById} targetBalloon
+ * @param {getElementById} targetSound
+ * @param {string} message
  */
-function showMessage(targetBalloon, message) {
+function showMessage(targetBalloon, targetSound, message) {
+    targetSound.currentTime = 0;
+    targetSound.play();
     targetBalloon.style.opacity = "1";
     targetBalloon.innerText = message;
-    setTimeout(hideMessage, 2 * sec, targetBalloon)
+    setTimeout(hideMessage, 4 * sec, targetBalloon, targetSound);
 }
-
+ 
 /**
- * Set the opacity to 0
- * @param {string} targetBalloon 
+ * Sets the opacity to 0 so you don't see it anymore
+ * @param {getElementById} targetBalloon
+ * @param {getElementById} targetSound
  */
-function hideMessage(targetBalloon) {
+function hideMessage(targetBalloon, targetSound) {
+    targetSound.pause();
     targetBalloon.style.opacity = "0";
 }
-
-
-
-//showMessage("mainCharacterspeech");
-// showMessage("Counterspeech");
-setTimeout(showMessage, 1 * sec, mainCharacterSpeech, "hello");
-setTimeout(showMessage, 2 * sec, counterSpeech, "hello");
-
+ 
+//showMessage("mainCharacterSpeech");
+//showMessage("counterSpeech");
+//setTimeout(showMessage, 1 * sec, mainCharacterSpeech, "hey what's up");
+//setTimeout(showMessage, 2 * sec, counterSpeech, "Yo buddy");
+ 
+showMessage()
+ 
